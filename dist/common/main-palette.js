8664a690888a9a60b7f60e1a5a1ad08d78e38d4a
a5hk <5412540+a5hk@users.noreply.github.com>
2022-06-03 18:07:30 +0430

    removed generator

--- dist/common/main-palette.js
+++
@@ -1,6 +0,0 @@
-import { colorPaletteFactory, registerPalette } from "./colors.js";
-function mainVariant() {
-    const p = new (colorPaletteFactory())("Night Coder", "Main");
-    return p;
-}
-registerPalette(mainVariant());
